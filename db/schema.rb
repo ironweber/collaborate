36415ac999074aa5446f273329a33f844b935931
Jamie Weber <jamieweb@gmail.com>
2014-08-10 01:20:43 -0700

    styling updates, sharing works

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140809232353) do
+ActiveRecord::Schema.define(version: 20140810080545) do
+
+  create_table "items", force: true do |t|
+    t.string   "description"
+    t.string   "url"
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
