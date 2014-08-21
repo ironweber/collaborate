35d141016db126e10b851ebd2e98166fd9f8776e
Jamie Weber <jamieweb@gmail.com>
2014-08-20 22:42:07 -0700

    Connected to the Backbone JS

--- app/assets/javascripts/collab.js
+++ app/assets/javascripts/collab.js
@@ -1,41 +1,94 @@
 (function($){
-	var CollabView = Backbone.View.extend({
-		initialize: function(attrs) {
-			this.template = attrs.template;
-			this.render();
+	window.Collab = {
+		init: function() {
+			$(function(){
+				var newItemForm = new Collab.Views.NewItem(),
+					items = new Collab.Collections.Items();
+				
+				items.fetch({
+					success: function(items) {
+			    		var itemsView = new Collab.Views.Items({ collection: items });
+			    		$('div.items').append(itemsView.render().el);
+			  		}
+			  	});
+			});
 		},
-		render: function() {
-			// this.$el.find('.items').html( this.template(this.model.toJSON()) );
-		},
+		Models: {},
+		Views: {},
+		Collections: {}
+	};
+
+	Collab.Views.NewItem = Backbone.View.extend({
+		el: '.new-item-form',
 		events: {
             "click [data-action='new-item']": "newItem"
         },
         newItem: function(e) {
-        	var newItem = new CollabItemModel({
+        	var newItem = new Collab.Models.Item({
     			'url': $(e.target).prev().prev('div').find('input').val(),
 				'description': $(e.target).prev('div').find('input').val()
     		});
-    		// console.log(newItem);
-        	// return false;
+
     		// save the new item
         	if(newItem.save()) {
         		// add the new item to the page
-        		this.$el.find('.items').prepend( this.template(newItem.toJSON()) );
+        		// this.$el.find('.items').prepend( this.template(newItem.toJSON()) );
         	}
         }
 	});
 
-	var CollabItemModel = Backbone.Model.extend({
-		initialize: function() { },
-		url: 'items'
+
+	Collab.Views.Item = Backbone.View.extend({
+		tagName: 'li',
+		render: function() {
+			this.$el.html( this.model.get('url') );
+			return this;
+		},
+		events: {
+            "click [data-action='destroy-item']": "destroyItem"
+        },
+        
+        destroyItem: function(e) {
+        	this.remove();
+        	this.unbind();
+        	// $(e.target).closest('.row').remove();
+ 		 	// this.$el.remove();
+        }
 	});
 
-	$(function(){
-		var template = Handlebars.compile($("#item-template").html());
-
-	 	var cv = new CollabView({
-	 		el: $(document),
-	 		template: template
-	 	});
+	Collab.Views.Items = Backbone.View.extend({
+		tagName: 'ul',
+		render: function() {
+			this.collection.each( this.addOne, this );
+			return this;
+		},
+		addOne: function(item) {
+			var item = new Collab.Views.Item({ model: item });
+			this.$el.append( item.render().el );
+		}
 	});
+
+	Collab.Models.Item = Backbone.Model.extend({
+		defaults: {
+			url: 'http://youneedaurl.com',
+			description: 'youneed a description'
+		},
+
+		validate: function(attrs) {
+			if(!attrs.url || !attrs.description) {
+				return 'Both Inputs are Required!';
+			}
+		},
+		url: '/items'
+
+	});
+
+	Collab.Collections.Items = Backbone.Collection.extend({
+		model: Collab.Models.Item,
+		url: '/items'
+	});
+
+
+
+	Collab.init();
 }(jQuery));
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,7 @@ class ItemsController < ApplicationController
   # GET /items.json
   def index
     @items = Item.all
+    render :json => @items
   end
 
   # GET /items/1
